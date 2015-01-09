fdfaddb0b8ea35c9c45762ab28b7e1774eb9816b
Matt Reilly <matt@matteroo.com>
2015-01-08 21:42:44 -0500

    Added ability to toggle checkmarks

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -23,7 +23,7 @@ class ChecklistViewController: UITableViewController {
 
     override func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
-            return 5
+            return 100
     }
     
     override func tableView(tableView: UITableView,
@@ -35,22 +35,33 @@ class ChecklistViewController: UITableViewController {
             
             let label = cell.viewWithTag(1000) as UILabel
             
-            if indexPath.row == 0 {
+            if indexPath.row % 5 == 0 {
                 label.text = "Walk the dog"
-            } else if indexPath.row == 1 {
+            } else if indexPath.row % 5 == 1 {
                 label.text = "Brush my teeth"
-            } else if indexPath.row == 2 {
+            } else if indexPath.row % 5 == 2 {
                 label.text = "Learn iOS development"
-            } else if indexPath.row == 3 {
+            } else if indexPath.row % 5 == 3 {
                 label.text = "Soccer practice"
-            } else if indexPath.row == 4 {
+            } else if indexPath.row % 5 == 4 {
                 label.text = "Eat ice cream"
             }
-
-
             
             return cell
     }
-
+    
+    override func tableView(tableView: UITableView,
+        didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            
+            if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+                if cell.accessoryType == .None {
+                    cell.accessoryType = .Checkmark
+                } else {
+                    cell.accessoryType = .None
+                }
+            }
+        
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
 
