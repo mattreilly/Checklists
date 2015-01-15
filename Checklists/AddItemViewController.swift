c171e7142e80d255d71b45e9d89553a852145311
Matt Reilly <matt@matteroo.com>
2015-01-15 11:41:03 -0500

    Only enable 'done' buttons in 'add item' screen if text field is non-empty

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    
     @IBAction func cancel() {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func done() {
+        println("Contents of the text field: \(textField.text)")
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     // make sure a user can't select the 'Add Item' row without 
@@ -26,9 +36,24 @@ class AddItemViewController: UITableViewController {
             return nil
     }
     
+    // fix the 'ambiguous row height' warning
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.rowHeight = 44
+    }
+    
+    func textField(textField: UITextField,
+        shouldChangeCharactersInRange range: NSRange,
+        replacementString string: String) -> Bool {
+            
+            let oldText: NSString = textField.text
+            let newText: NSString =
+                oldText.stringByReplacingCharactersInRange(range,
+                    withString: string)
+            
+            doneBarButton.enabled = newText.length > 0
+            
+            return true
     }
 }
